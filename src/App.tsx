785492742cbdf6cd66bf4f3dc3ad894cb91ba427
Dmytro Hryshyn <dmytro@codemod.com>
2024-06-05 18:35:10 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useVariableValue, withDevCycleProvider } from '@devcycle/react-client-sdk'
+
+const useFlag = useVariableValue;
 
 function App() {
+  const var1 = useFlag('togglebot-speed', 'default');
+  const var2 = useFlag('example-text', 'default');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     togglebot-speed: {var1} &nbsp;
+     example-text: {var2}
     </div>
   );
 }
 
-export default App;
+export default withDevCycleProvider({ sdkKey: process.env.REACT_APP_DVC ?? ''})(App);
+ 
